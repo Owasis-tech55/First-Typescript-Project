da39c955856f8e94951c19386b9ce77a2e37f6c3
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-03-05 13:27:23 +0500

    Update main.js

--- typescript Assigment/ex_40 Functions/main.js
+++ typescript Assigment/ex_40 Functions/main.js
@@ -1,9 +1,20 @@
-var make_album = function (artist, album, track) {
-    return "{".concat(artist, " : ").concat(album, " }");
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-var artist1 = make_album("Nazia hassan", "Disco Deewane");
-var artist2 = make_album("Atif Aslam", "Meri kahani");
-var artist3 = make_album("NFAK", "Shah-e-jahan");
-console.log(artist1);
-console.log(artist2);
-console.log(artist3);
+function make_Album(artist, title, tracks) {
+    return __assign({ artist: artist, title: title }, (tracks !== undefined && { tracks: tracks }));
+}
+var album1 = make_Album("Nazia hassan", "Disco Deewane");
+var album2 = make_Album("Atif Aslam", "Meri kahani");
+var album3 = make_Album("NFAK", "Shah-e-jahan", 55);
+console.log(album1);
+console.log(album2);
+console.log(album3);
