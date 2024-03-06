263cd6f9be085d4209db53139e326b9ed21f8c05
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-03-06 09:21:13 +0500

    Update main.js

--- typescript Assigment/ex_3 NameCases/main.js
+++ typescript Assigment/ex_3 NameCases/main.js
@@ -4,4 +4,10 @@ console.log(playerName.toUpperCase());
 //lowercase
 console.log(playerName.toLowerCase());
 //titlecase
+//Not the recommanded method
 console.log(playerName.replace(/\b\w/g, function (c) { return c.toUpperCase(); }));
+var step1 = playerName.split(" ");
+var step2 = step1.map(function (word) { return word.charAt(0).toUpperCase() + word.
+    substr(1).toLowerCase(); });
+var step3 = step2.join("  ");
+console.log("Titlecase: ".concat(step3));
