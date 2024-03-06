ddc9e4f9935f0720d4330f8bd84ab53a15af4f19
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-03-06 07:35:58 +0500

    Update main.ts

--- typescript Assigment/ex_33 ordinal numbers/main.ts
+++ typescript Assigment/ex_33 ordinal numbers/main.ts
@@ -1,8 +1,9 @@
 
+
 let ordinalnum: Array<number> = [1,2,3,4,5,6,7,8,9];
 
 
-for(let i = 0 ; i<users.length ; i++) {
+for(let i = 0 ; i<ordinalnum.length ; i++) {
         if(ordinalnum[i] === 1) {
         console.log(`${ordinalnum[i]}st`)
 } else if(ordinalnum[i] === 2){
@@ -13,4 +14,5 @@ for(let i = 0 ; i<users.length ; i++) {
     console.log(`${ordinalnum[i]}th`)
 }
 }
+
 
