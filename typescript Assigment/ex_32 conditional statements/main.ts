dba45625f1e61df4a746d4601b7c008122527edf
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-03-12 16:50:57 +0500

    Update main.ts

--- typescript Assigment/ex_32 conditional statements/main.ts
+++ typescript Assigment/ex_32 conditional statements/main.ts
@@ -1,10 +1,12 @@
-
 let currentUsers: Array<string> = ["Owaiskhilji", "AdeenaAsghar", "Admin", "user420", "unknownuser"];
-let newUsers: Array<string> = ["Owaiskhilji", "Papakipari", "ADnanKK", "user420", "Rakesh786"];
-let i = 0;
-for(let i =0; i<currentUsers.length ; i++)
-if(currentUsers[i]=== newUsers[i]){
-    console.log("Username not avaible")
-} else {
-    console.log("Username available")
+let newUsers: Array<string> = [ "UsEr420", "Papakipari", "ADnanKK", "owaiskhilji","Rakesh786"];
+
+for (let newUser of newUsers) {
+    let lowerNewUser = newUser.toLowerCase();
+
+    if (currentUsers.some(user => user.toLowerCase() === lowerNewUser)) {
+        console.log(`${newUser} not available`);
+    } else {
+        console.log(`${newUser} available`);
+    }
 }
