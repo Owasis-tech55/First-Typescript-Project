321baa8c949ae756f291ab7ea8d9cd387889e193
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-04-16 22:11:27 +0500

    Update main.ts

--- typescript Assigment/ex_27 conditional statements #4/main.ts
+++ typescript Assigment/ex_27 conditional statements #4/main.ts
@@ -1,15 +1,49 @@
 
 
 
-let alien_Color: string = "Black"
 
-if(alien_Color === "Red") {
+let alien_Color: string = "Green"
+
+if(alien_Color === "Green") {
     console.log("You have earned 5 poinrs")
 
-} else if(alien_Color === "Green") {
+} else if(alien_Color === "Yellow") {
     console.log("you have earned 10 points")
 
 } else if(alien_Color === "Black") {
+    console.log("you have earned 20 points")
+}
+else {
+    console.log("you have earned nothing")
+}
+
+
+
+ alien_Color = "Yellow"
+
+if(alien_Color === "Green") {
+    console.log("You have earned 5 poinrs")
+
+} else if(alien_Color === "Yellow") {
+    console.log("you have earned 10 points")
+
+} else if(alien_Color === "Black") {
+    console.log("you have earned 20 points")
+}
+else {
+    console.log("you have earned nothing")
+}
+
+
+alien_Color = "Red"
+
+if(alien_Color === "Green") {
+    console.log("You have earned 5 poinrs")
+
+} else if(alien_Color === "Yellow") {
+    console.log("you have earned 10 points")
+
+} else if(alien_Color === "Red") {
     console.log("you have earned 20 points")
 }
 else {
