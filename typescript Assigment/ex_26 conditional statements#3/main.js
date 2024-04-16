5304c0eb5421729d49394fe8af9299b1120c3b09
Owasis-tech55 <160662560+Owasis-tech55@users.noreply.github.com>
2024-04-16 22:07:24 +0500

    Update main.js

--- typescript Assigment/ex_26 conditional statements#3/main.js
+++ typescript Assigment/ex_26 conditional statements#3/main.js
@@ -1,7 +1,15 @@
-var alien_Color = "Black";
+let alien_Color = "Black";
 if (alien_Color === "Green") {
     console.log("You have earned 5 poinrs");
 }
 else {
     console.log("you have earned 10 points");
 }
+alien_Color = "Green";
+if (alien_Color === "Green") {
+    console.log("You have earned 5 poinrs");
+}
+else {
+    console.log("you have earned 10 points");
+}
+export {};
